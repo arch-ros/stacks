31f3c1efa4f3606e9a90204ddf06554560002fe4
Daniel Pfrommer <dan.pfrommer@gmail.com>
2019-05-02 22:40:11 -0400

    Changed oyster website branding

--- test.py
+++
@@ -1,9 +0,0 @@
-from oyster.pacman.pkgbuild import PkgBuild
-
-pkg = PkgBuild('/home/oyster/packages/archlinux/systemd/repos/core-x86_64/PKGBUILD')
-print(list(map(lambda p: str(p), pkg.packages)))
-# Get the package dependencies
-pkg = PkgBuild('/home/oyster/packages/archlinux/man-db/repos/core-x86_64/PKGBUILD')
-mandb_pkg = pkg.packages[0]
-pkg = PkgBuild('/home/oyster/packages/archlinux/linux/repos/core-x86_64/PKGBUILD')
-print(list(map(lambda p: str(p), pkg.packages)))
